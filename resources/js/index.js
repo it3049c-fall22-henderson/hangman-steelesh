ad79cd2e741fca22fd5e109545d6c8841ecc1df5
Steele <shrevesa@mail.uc.edu>
2022-10-12 19:51:58 -0400

    Allow user to enter name and validate inputs

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,46 +1,118 @@
-// START + DIFFICULTY SELECTION
 const startWrapper = document.getElementById(`startWrapper`);
 const difficultySelectForm = document.getElementById(`difficultySelect`);
 const difficultySelect = document.getElementById(`difficulty`);
-
-// GAME
 const gameWrapper = document.getElementById(`gameWrapper`);
 const guessesText = document.getElementById(`guesses`);
 const wordHolderText = document.getElementById(`wordHolder`);
-
-// GUESSING FORM
 const guessForm = document.getElementById(`guessForm`);
 const guessInput = document.getElementById(`guessInput`);
 const guessButton = document.getElementById(`guessSubmitButton`);
-// GAME RESET BUTTON
 const resetGame = document.getElementById(`resetGame`);
-
-// CANVAS
 let canvas = document.getElementById(`hangmanCanvas`);
+const nameInput = document.getElementById(`my-name-input`);
+const saveNameForm = document.getElementById(`nameInput`);
+const saveNameBtn = document.getElementById(`save`);
+const startGameBtn = document.getElementById(`start`);
 
-// The following Try-Catch Block will catch the errors thrown
 try {
-  // Instantiate a game Object using the Hangman class.
   const game = new Hangman(canvas);
-  // add a submit Event Listener for the to the difficultySelectionForm
-  difficultySelectForm.addEventListener(`submit`, function (event) {
-    event.preventDefault();
-    //    get the difficulty input
-    let gameDifficulty = difficultySelect.value;
-    //    call the game start() method, the callback function should do the following
-    game.start(gameDifficulty, function () {
-      event.preventDefault();
-      //       1. hide the startWrapper
-      startWrapper.classList.add("hidden");
-      //       2. show the gameWrapper
-      gameWrapper.classList.remove("hidden");
-      //       3. call the game getWordHolderText and set it to the wordHolderText
-      wordHolderText.innerHTML = game.getWordHolderText();
-      //       4. call the game getGuessesText and set it to the guessesText
-      guessesText.innerHTML = game.getGuessesText();
-    });
+
+  $(nameInput).keyup(function () {
+    if ($(this).val().length != 0) {
+      saveNameBtn.disabled = false;
+      saveNameBtn.style.backgroundColor = "#007BFF";
+      saveNameBtn.style.color = "#fff";
+    } else {
+      saveNameBtn.disabled = true;
+      saveNameBtn.style.backgroundColor = "#fff";
+      saveNameBtn.border = "1px solid gray";
+      saveNameBtn.style.color = "gray";
+    }
+  });
+  saveNameBtn.addEventListener(`click`, function (event) {
+    if (nameInput.value === "") {
+      alert(`User must enter a name`);
+      throw Error(`User must enter a name`);
+    } else if (!/^[A-Za-z\s]*$/.test(nameInput.value)) {
+      alert(`Must be a valid name - "${nameInput.value}" is not a valid input`);
+      throw Error(
+        `Must be a valid name - "${nameInput.value}" is not a valid input`
+      );
+    } else {
+      sessionStorage.setItem("Name", nameInput.value);
+      nameInput.innerHTML = nameInput.value;
+      saveNameBtn.value = "Saved!";
+      saveNameBtn.style.color = "white";
+      saveNameBtn.style.backgroundColor = "#28A745";
+      saveNameBtn.style.border = "1px solid gray";
+      difficultySelect.disabled = false;
+      $(nameInput).keyup(function () {
+        if ($(this).val().length != 0) {
+          saveNameBtn.value = `Modify current name (${sessionStorage.getItem(
+            "Name"
+          )})`;
+          saveNameBtn.style.backgroundColor = "#FFC107";
+          saveNameBtn.style.color = "#fff";
+          difficultySelect.value = "Select Difficulty";
+          difficultySelect.disabled = true;
+          startGameBtn.disabled = true;
+        } else {
+          saveNameBtn.value = "modify";
+          saveNameBtn.disabled = true;
+          saveNameBtn.style.backgroundColor = "#fff";
+          saveNameBtn.border = "1px solid gray";
+          saveNameBtn.style.color = "gray";
+          difficultySelect.value = "Select Difficulty";
+          difficultySelect.disabled = true;
+          startGameBtn.disabled = true;
+        }
+      });
+    }
   });
 
+  $(difficultySelect).change(function () {
+    var theVal = $(this).val();
+    switch (theVal) {
+      case "Select Difficulty":
+        startGameBtn.disabled = true;
+        break;
+      case "easy":
+        startGameBtn.disabled = false;
+        break;
+      case "medium":
+        startGameBtn.disabled = false;
+        break;
+      case "hard":
+        startGameBtn.disabled = false;
+        break;
+    }
+  });
+
+  startGameBtn.addEventListener(`click`, function (event) {
+    if (
+      difficultySelect.selectedIndex === 0 &&
+      sessionStorage.getItem("Name") == null
+    ) {
+      alert(`Must save a name AND choose a difficulty to start the game`);
+      throw Error(`Must save a name AND choose a difficulty to start the game`);
+    } else if (sessionStorage.getItem("Name") == null) {
+      alert(`Must save a name to start the game`);
+      throw Error(`Must save a name to start the game`);
+    } else if (difficultySelect.selectedIndex === 0) {
+      alert(`Must select a game difficulty`);
+      throw Error(`Must select a game difficulty`);
+    } else {
+      event.preventDefault();
+      let gameDifficulty = difficultySelect.value;
+      game.start(gameDifficulty, function () {
+        event.preventDefault();
+        startWrapper.classList.add("hidden");
+        gameWrapper.classList.remove("hidden");
+        wordHolderText.innerHTML = game.getWordHolderText();
+        guessesText.innerHTML = game.getGuessesText();
+      });
+    }
+  });
   // add a submit Event Listener to the guessForm
   guessForm.addEventListener(`submit`, function (e) {
     e.preventDefault();
@@ -87,3 +159,11 @@ try {
   console.error(error);
   alert(error);
 }
+window.onload = function () {
+  difficultySelect.selectedIndex = 0;
+  difficultySelect.disabled = true;
+  saveNameBtn.disabled = true;
+  startGameBtn.disabled = true;
+  nameInput.value = "";
+  sessionStorage.clear();
+};
