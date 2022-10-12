5144b9c18b97c559955a13fa68ab0b8488276827
Steele <shrevesa@mail.uc.edu>
2022-10-11 22:41:30 -0400

    Complete difficulty event listener

--- resources/js/index.js
+++ resources/js/index.js
@@ -21,29 +21,43 @@ let canvas = document.getElementById(`hangmanCanvas`);
 // The following Try-Catch Block will catch the errors thrown
 try {
   // Instantiate a game Object using the Hangman class.
-
+  game = new Hangman(canvas);
   // add a submit Event Listener for the to the difficultySelectionForm
-  //    get the difficulty input
-  //    call the game start() method, the callback function should do the following
-  //       1. hide the startWrapper
-  //       2. show the gameWrapper
-  //       3. call the game getWordHolderText and set it to the wordHolderText
-  //       4. call the game getGuessessText and set it to the guessesText
-  difficultySelectForm.addEventListener(`submit`, function (event) {});
+  difficultySelectForm.addEventListener(`submit`, function (event) {
+    event.preventDefault();
+    //    get the difficulty input
+    let gameDifficulty = difficultySelect.value;
+    //    call the game start() method, the callback function should do the following
+    game.start(gameDifficulty, function () {
+      event.preventDefault();
+      //       1. hide the startWrapper
+      startWrapper.classList.add("hidden");
+      //       2. show the gameWrapper
+      gameWrapper.classList.remove("hidden");
+      //       3. call the game getWordHolderText and set it to the wordHolderText
+      wordHolderText.innerHTML += game.getWordHolderText();
+      //       4. call the game getGuessessText and set it to the guessesText
+      guessesText.innerHTML += game.getGuessesText();
+    });
+  });
 
   // add a submit Event Listener to the guessForm
-  //    get the guess input
-  //    call the game guess() method
-  //    set the wordHolderText to the game.getHolderText
-  //    set the guessesText to the game.getGuessesText
-  //    clear the guess input field
-  // Given the Guess Function calls either the checkWin or the onWrongGuess methods
-  // the value of the isOver and didWin would change after calling the guess() function.
-  // Check if the game isOver:
-  //      1. disable the guessInput
-  //      2. disable the guessButton
-  //      3. show the resetGame button
-  // if the game is won or lost, show an alert.
+  guessForm.addEventListener(`submit`, function (event) {
+    event.preventDefault();
+    //    get the guess input
+    //    call the game guess() method
+    //    set the wordHolderText to the game.getHolderText
+    //    set the guessesText to the game.getGuessesText
+    //    clear the guess input field
+    // Given the Guess Function calls either the checkWin or the onWrongGuess methods
+    // the value of the isOver and didWin would change after calling the guess() function.
+    // Check if the game isOver:
+    //      1. disable the guessInput
+    //      2. disable the guessButton
+    //      3. show the resetGame button
+    // if the game is won or lost, show an alert.
+  });
+
   guessForm.addEventListener(`submit`, function (e) {});
 
   // add a click Event Listener to the resetGame button
